5ac331f6a750882711d63b504cd15f4025ae5370
LorenzoCappellieri <101276115+LorenzoCappellieri@users.noreply.github.com>
2022-04-28 10:48:55 +0200

    Add files via upload

---
+++ Codes/UnitaAbitativa.java
@@ -0,0 +1,10 @@
+public class UnitaAbitativa{
+private Indirizzo indirizzo;
+
+public UnitaAbitativa(Indirizzo indirizzo){
+    this.indirizzo = indirizzo;
+}
+
+public Indirizzo getindirizzo(){return indirizzo;}
+public void setindirizzo(Indirizzo indirizzo){this.indirizzo = indirizzo;}
+}
